a7376e28c92f5681c2f9a428b37d9bec6a0dc5bf
oduchenko <oduchenko@vibrenthealth.com>
2019-06-10 09:36:43 -0400

    fixed comments

--- src/main/java/com/pmt/health/workflows/UserAdminPage.java
+++ src/main/java/com/pmt/health/workflows/UserAdminPage.java
@@ -4,19 +4,15 @@ import com.pmt.health.interactions.application.App;
 import com.pmt.health.interactions.element.selenified.WebbElement;
 import com.pmt.health.objects.user.User;
 import com.pmt.health.utilities.LocatorType;
-import org.testng.log4testng.Logger;
 
 public class UserAdminPage {
 
     private final App app;
     private final WebbElement addUserButton;
     private final WebbElement loggedInHeadingAdmin;
-    private final WebbElement loggedInHeadingUser;
     private final WebbElement createdUser;
-    private final WebbElement headUserAdmin;
     private final WebbElement userAdminButton;
 
-    Logger log = Logger.getLogger(LoginPage.class);
     private User user;
 
     public UserAdminPage(App app, User user) {
@@ -24,8 +20,6 @@ public class UserAdminPage {
         this.user = user;
         this.addUserButton = app.newElement(LocatorType.CLASSNAME, "add-user-button");
         this.loggedInHeadingAdmin = app.newElement(LocatorType.XPATH, "//h1[text()='Reports']");
-        this.headUserAdmin = app.newElement(LocatorType.XPATH, "//h1[text()='User Administration']");
-        this.loggedInHeadingUser = app.newElement(LocatorType.XPATH, "//h1[text()='Dashboard']");
         this.createdUser = app.newElement(LocatorType.XPATH, "//div[contains(text(), \"" + user.getEmail() + "\")]");
         this.userAdminButton = app.newElement(LocatorType.CSS, "ul li:nth-of-type(4) a");
     }
